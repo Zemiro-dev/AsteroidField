828e3cfb40a0fa25e05b31fac451fe4944483ae8
Logan Wilkerson <lcwilker@gmail.com>
2025-12-14 19:48:18 -0500

    moving ui screen

---
+++ game/ui/player_resource_ui.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+class_name PlayerResourceUi
+
+
+@onready var health: TextureProgressBar = $Health
+@onready var boost: TextureProgressBar = $Boost
+@onready var xp: TextureProgressBar = $Xp
+@onready var level: TextureProgressBar = $Level
+
+
+func bind_to_player(player: Player):
+	var damagable := GameActor.get_damagable(player)
+	if damagable:
+		health.value = ceil(float(damagable.current_health) / float(damagable.max_health) * health.max_value)
+		damagable.on_health_changed.connect(
+			func(new_health: int, max_health: int): 
+				health.value = ceil(float(new_health) / float(max_health) * health.max_value)
+		)
+	var levelable := GameActor.get_levelable(player)
+	if levelable:
+		level.value = (levelable.level - 1)
+		xp.value = ceil(float(levelable.xp) / float(levelable.max_xp) * xp.max_value)
+		levelable.on_xp_changed.connect(
+			func(current_xp: int, max_xp: int, current_level: int):
+				xp.value = ceil(float(current_xp) / float(max_xp) * xp.max_value)
+				level.value = current_level - 1
+		)
+	player.on_boost_duration_changed.connect(
+		func(new_duration: float, max_duration: float, _player: Player):
+			boost.value = ceil(float(new_duration) / float(max_duration) * boost.max_value)
+			if _player.is_boost_on_cd:
+				boost.modulate = Color(Color.WHITE, .25)
+			else: 
+				boost.modulate = Color.WHITE
+	)
--- game/world/debug/debug_world_a.gd
+++ game/world/debug/debug_world_a.gd
@@ -2,36 +2,21 @@ extends Node2D
 
 
 @onready var player: Player = $Player
-@onready var health: TextureProgressBar = $UI/Health
-@onready var boost: TextureProgressBar = $UI/Boost
-@onready var xp: TextureProgressBar = $UI/Xp
-@onready var level: TextureProgressBar = $UI/Level
 @onready var death_timer: Timer = $DeathTimer
 @onready var player_camera: PlayerCamera = $PlayerCamera
 @onready var upgrade_screen: UpgradeScreen = $UpgradeScreen
+@onready var ui: PlayerResourceUi = $UI
 
 
 func _ready() -> void:
 	var damagable := GameActor.get_damagable(player)
 	if damagable:
-		health.value = ceil(float(damagable.current_health) / float(damagable.max_health) * health.max_value)
-		damagable.on_health_changed.connect(
-			func(new_health: int, max_health: int): 
-				health.value = ceil(float(new_health) / float(max_health) * health.max_value)
-		)
 		damagable.on_death.connect(
 			func(_actor: Node2D):
 				death_timer.start()
 		)
 	var levelable := GameActor.get_levelable(player)
 	if levelable:
-		level.value = (levelable.level - 1)
-		xp.value = ceil(float(levelable.xp) / float(levelable.max_xp) * xp.max_value)
-		levelable.on_xp_changed.connect(
-			func(current_xp: int, max_xp: int, current_level: int):
-				xp.value = ceil(float(current_xp) / float(max_xp) * xp.max_value)
-				level.value = current_level - 1
-		)
 		levelable.on_level_up.connect(
 			func(_levelable: Levelable):
 				var options: Array[LevelableStatModifier]
@@ -52,7 +37,7 @@ func _ready() -> void:
 				else:
 					options = [LevelableStatModifier.ProjectileAttackSpeedUpPerLevel.new()]
 				 
-				if !options.is_empty():					
+				if !options.is_empty():
 					get_tree().paused = true
 					upgrade_screen.render_for_options(options)
 		)
@@ -71,14 +56,6 @@ func _ready() -> void:
 					
 				get_tree().paused = false
 		)
-	player.on_boost_duration_changed.connect(
-		func(new_duration: float, max_duration: float, _player: Player):
-			boost.value = ceil(float(new_duration) / float(max_duration) * boost.max_value)
-			if _player.is_boost_on_cd:
-				boost.modulate = Color(Color.WHITE, .25)
-			else: 
-				boost.modulate = Color.WHITE
-				
-	)
 	death_timer.timeout.connect(func(): GameManager.change_scene_to_main_menu())
 	player_camera.player = player
+	ui.bind_to_player(player)
